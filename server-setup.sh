15c54ae01c65da2484822a7a4d6e03caf59dbf0a
Aaron Isaac <aaron@aisaac.co>
2023-12-25 17:45:53 -0800

    change install order

--- server-setup.sh
+++ server-setup.sh
@@ -2,16 +2,16 @@
 echo "Installing programs\n"
 sudo apt-get install neovim rsync git qemu-guest-agent zsh
 
+# run the guest agent
+echo "Starting guest agent\n"
+sudo systemctl start qemu-guest-agent
+sudo systemctl enable qemu-guest-agent
+
 # set up Zsh
 echo "Setting up Zsh\n"
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm ~/.zshrc
 curl -fsSL https://raw.githubusercontent.com/ajisaac/server-setup/main/.zshrc > ~/.zshrc
-
-# run the guest agent
-echo "Starting guest agent\n"
-sudo systemctl start qemu-guest-agent
-sudo systemctl enable qemu-guest-agent
 
 # copy the dotfiles into appropriate place
