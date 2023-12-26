561b8aff699c68114dba5bc028cad97df989b0d7
Aaron Isaac <aaron@aisaac.co>
2023-12-25 17:31:16 -0800

    add newline

--- server-setup.sh
+++ server-setup.sh
@@ -1,13 +1,16 @@
 # starter programs
+echo "Installing programs\n"
 sudo apt-get install neovim rsync git qemu-guest-agent zsh
 
 # set up Zsh
+echo "Setting up Zsh\n"
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm ~/.zshrc
 curl -fsSL https://raw.githubusercontent.com/ajisaac/server-setup/main/.zshrc > ~/.zshrc
 
 # run the guest agent
+echo "Starting guest agent\n"
 sudo systemctl start qemu-guest-agent
 sudo systemctl enable qemu-guest-agent
 
